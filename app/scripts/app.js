a829d827414e3b0d8fb1c4488494995e0700be77
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2016-11-25 19:56:31 +0400

    index

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,8 +17,66 @@ jQuery(document).ready(function($){
         });
     }
 
-    reveal('#scrn-3 .cell');
-    reveal('#scrn-2 .climateInfo');
+    if($('#scrn-3 .cell').length > 0)
+        reveal('#scrn-3 .cell');
+
+    if($('#scrn-2 .climateInfo').length > 0)
+        reveal('#scrn-2 .climateInfo');
+
+    if($('#daoscheme_dots').length > 0)
+        reveal('#daoscheme_dots');
+
+    if($('#scrn-5 .sprite-daoOperator').length > 0)
+        reveal('#scrn-5 .sprite-daoOperator');
+
+    if($('#scrn-6 .sprite-daoCodeTwo').length > 0)
+        reveal('#scrn-6 .sprite-daoCodeTwo');
+
+
+    
+    /*INDEX, DAO SCHEME ANIMATION*/
+    var timer = true;
+
+    function moveSheme(){
+
+        if(timer == true){
+            $o = $('#daoscheme');
+            ofst = $w.scrollTop() - $o.offset().top;
+            p = ofst / ($w.height()/100);
+
+            console.log('scrolltop ' + $w.scrollTop());
+            console.log('ofst ' + ofst);
+            console.log('p ' + p);
+
+
+            if ( 30 < p )
+                $o.removeClass('m0 m1 m2 m3 m4 m6').addClass('m5');
+            if ( (0 < p) && (p < 30) )
+                $o.removeClass('m0 m1 m2 m3 m5 m6').addClass('m4');
+            if ( (-30 < p) && (p < -20) )
+                $o.removeClass('m0 m1 m2 m4 m5 m6').addClass('m3');
+            if ( (-60 < p) && (p < -50) )
+                $o.removeClass('m0 m1 m3 m4 m5 m6').addClass('m2');
+            if ( (-80 < p) && (p < -70) )
+                $o.removeClass('m0 m2 m3 m4 m5 m6').addClass('m1');
+            if ( (-100 < p) && (p < -90) )
+                $o.removeClass('m1 m2 m3 m4 m5 m6').addClass('m0');
+        }
+
+        timer = false;
+        setTimeout(function(){timer = true}, 150);
+
+    }
+
+    if($('#daoscheme').length > 0){
+        $w.bind('scroll', moveSheme);
+    }
+    /*end of INDEX, DAO SCHEME ANIMATION*/
+
+
+
+
+
     /*===  end CHECK IF ELEMENT IS VISIBLE ===*/
 
 
