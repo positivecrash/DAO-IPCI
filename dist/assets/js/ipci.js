00d5049c8642b0ffb2ec3238450a62846776dce7
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2016-12-06 14:39:25 +0400

    ie fix

--- dist/assets/js/ipci.js
+++ dist/assets/js/ipci.js
@@ -5067,15 +5067,50 @@ jQuery(document).ready(function($){
     }
 
 
-    /*--- Dropdowns for old browsers ---*/
-    if (!Modernizr.csspointerevents) {
-      $('.dropdown:not(:focus)').children('.dropdown__toggle').on('click', function(e){
-        console.log('click');
-        e.preventDefault();
-        e.stopPropagation();
-      });
+    /*--- Dropdowns for old IE ---*/
+    /*I use here browser detection because Modernizr doesn't detects pointer events for non-svg elements and it doesn't work for IE9 and IE10*/
+
+    /**
+     * detect IE
+     * returns version of IE or false, if browser is not Internet Explorer
+     */
+    function detectIE() {
+      var ua = window.navigator.userAgent;
+
+      var msie = ua.indexOf('MSIE ');
+      if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+      }
+
+      var trident = ua.indexOf('Trident/');
+      if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+      }
+
+      var edge = ua.indexOf('Edge/');
+      if (edge > 0) {
+        // Edge (IE 12+) => return version number
+        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+      }
+
+      // other browser
+      return false;
     }
 
+
+    if($('.dropdown').length > 0){
+        if(detectIE() < 11){
+            $('.dropdown__toggle').on('click', function(e){
+                e.preventDefault();
+                e.stopPropagation();
+
+                $(this).next().toggleClass('show');
+            });
+        }
+    }
 
 
 
